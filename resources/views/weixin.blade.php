b9ffffac5f348429b0ab615d11a98e6e3155c8ec
dujianjun <v@jianwi.cn>
2020-05-01 11:51:51 +0800

    界面友好的查询

--- resources/views/weixin.blade.php
+++ resources/views/weixin.blade.php
@@ -11,12 +11,13 @@
     <script src="https://cdn.bootcdn.net/ajax/libs/axios/0.19.2/axios.min.js"></script>
 </head>
 <style>
+    body{
+        font-size: 1.2em;
+    }
     #container {
         background: #f8f8f8;
         min-height: 100vh;
-        margin: 1%;
-        width: 98%;
-        border: 10px #e0bb8f double;
+        border: 20px #e0bb8f solid;
     }
 
     #search_box {
@@ -29,23 +30,31 @@
     #text {
         text-indent: 2em;
     }
+
+    #goButton{
+        font-size: 1.5rem;
+        font-weight: bolder;
+        width: 80px;
+        height: 80px;
+        border-radius: 50%;
+    }
 </style>
 <body style="background: white">
 <div id="container" class="container p-2">
     <div class="text-center">
-        <img style="width: 58px" class="img-sm" src="images/gold.png">
+        <img style="width: 78px" class="img-sm" src="images/gold.png">
     </div>
-    <div class="text-center mt-1 font-weight-bolder">
-        <p class="h2">
+    <div class="text-center mt-2 font-weight-bolder">
+        <p class="h1" style="font-weight: 800">
             荣誉证书
         </p>
         <p style="font-size: 0.75em">CERTIFICATE OF HONOR</p>
     </div>
     <div id="app">
-        <p class="mt-5">
+        <p class="mt-5 font-weight-bold">
             优秀的<input v-model="name" class="text-center" id="search_box"> 同学:
         </p>
-        <p class="my-4" id="text" v-show="!failMessage">
+        <p class="my-5 font-weight-bold" id="text" v-show="!failMessage">
             系统检测到您过于优秀,特此为您颁发了大量证书.请您前来领取.
         </p>
         <p v-show="failMessage" class="my-4 text-center">
@@ -53,8 +62,8 @@
             <br>
             是不是名字写错了?
         </p>
-        <p class="text-center" v-show="!certificates">
-            <button class="btn btn-success" @click="search">一键查询</button>
+        <p class="text-center pt-5" v-show="!certificates">
+            <button id="goButton" class="btn mt-3 btn-success" @click="search">Go</button>
         </p>
         <div id="certificates_table" v-show="certificates">
 
