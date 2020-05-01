01bd716821f9194f5f1bc062e20b9a6afa29eaf2
dujianjun <v@jianwi.cn>
2020-05-01 11:25:59 +0800

    有趣的证书查询页面

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,9 +12,9 @@
 <div id="app">
 
 </div>
-<div class="my-3 text-center w-100 font-weight-bolder">
-    Designed By <a href="http://jialidun.vip">Jianwi</a>@2020
-</div>
+{{--<div class="my-3 text-center w-100 font-weight-bolder">--}}
+{{--    Designed By <a href="http://jialidun.vip">Jianwi</a>@2020--}}
+{{--</div>--}}
 <script !src="">
     const APP_URL = '{{ config('app.url') }}'
     // console.log(APP_URL)
