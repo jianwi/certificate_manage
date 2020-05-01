f2e829a1f2ac3a286e16ef878136caa7820aacce
dujianjun <v@jianwi.cn>
2020-05-01 12:24:41 +0800

    优化

--- resources/js/front/ component/CertificateInfo.vue
+++ resources/js/front/ component/CertificateInfo.vue
@@ -1,7 +1,13 @@
 <template>
-    <div ref="cer_container" class="mt-3" v-html="certificate">
+    <div>
+        <div ref="cer_container" class="mt-3" v-html="certificate">
 
+        </div>
+        <div class="text-center" checked="my-5" style="margin-bottom: 50px" v-show="!isPrint">
+            <button class="btn btn-success" @click="print">一键打印</button>
+        </div>
     </div>
+
 </template>
 
 <script>
@@ -9,8 +15,18 @@
         name: "CertificateInfo",
         data() {
             return {
-                certificate: ""
+                certificate: "",
+                isPrint: false
             }
+        },
+        methods:{
+          print(){
+              this.isPrint = true
+              setTimeout(()=>{
+                  window.print()
+              },300)
+
+          }
         },
         mounted() {
             this.$http.get(this.$url + '/certificates/' + this.$route.params.code,).then(res => {
@@ -27,17 +43,17 @@
                     // console.log("第" + i + "次：", template)
                 }
                 this.certificate = template
-                setTimeout(()=>{
-                    let cer = document.getElementById('cer')
-                    let container = this.$refs.cer_container
-                    let container_w = container.clientWidth
-                    let cer_w = cer.clientWidth
-                    console.log(cer_w)
-                    if(container_w < cer_w){
-                        let scale = Math.floor((container_w / cer_w)*100)/100
-                        cer.style.transform = 'scale('+scale+')'
-                    }
-                },100);
+                // setTimeout(()=>{
+                //     let cer = document.getElementById('cer')
+                //     let container = this.$refs.cer_container
+                //     let container_w = container.clientWidth
+                //     let cer_w = cer.clientWidth
+                //     console.log(cer_w)
+                //     if(container_w < cer_w){
+                //         let scale = Math.floor((container_w / cer_w)*100)/100
+                //         cer.style.transform = 'scale('+scale+')'
+                //     }
+                // },100);
 
             })
         }
--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,7 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
     <title>证书管理系统</title>
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
 </head>
 <body class="position-relative">
 <div id="app">
